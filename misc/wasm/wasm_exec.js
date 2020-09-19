568c096b5eec89b15c824237dc0a8a3f3a2e1cc2
Andreas Bergmeier <abergmeier@gmx.net>
2020-09-19 09:50:05 +0200

    misc/wasm: Polyfill TextEncoder/TextDecoder for GJS

--- misc/wasm/wasm_exec.js
+++ misc/wasm/wasm_exec.js
@@ -100,6 +100,155 @@
 		if (!global.fs) {
 			global.fs = polyfillFilesystem()
 		}
+		const polyfillTextEncoder = () => {
+			// Shameless plug from https://github.com/samthor/fast-text-encoding/blob/master/text.js
+			/**
+			 * @constructor
+			 */
+			function GLibTextEncoder() {
+				// This does not accept an encoding, and always uses UTF-8:
+				//   https://www.w3.org/TR/encoding/#dom-textencoder
+			}
+			Object.defineProperty(GLibTextEncoder.prototype, 'encoding', {value: 'utf-8'});
+			/**
+			 * @param {string} string
+			 * @param {{stream: boolean}=} options
+			 * @return {!Uint8Array}
+			 */
+			GLibTextEncoder.prototype['encode'] = function(string, options={stream: false}) {
+				if (options.stream) {
+					throw new Error(`Failed to encode: the 'stream' option is unsupported.`);
+				}
+
+				let pos = 0;
+				const len = string.length;
+
+				let at = 0;  // output position
+				let tlen = Math.max(32, len + (len >>> 1) + 7);  // 1.5x size
+				let target = new Uint8Array((tlen >>> 3) << 3);  // ... but at 8 byte offset
+
+				while (pos < len) {
+					let value = string.charCodeAt(pos++);
+					if (value >= 0xd800 && value <= 0xdbff) {
+						// high surrogate
+						if (pos < len) {
+							const extra = string.charCodeAt(pos);
+							if ((extra & 0xfc00) === 0xdc00) {
+								++pos;
+								value = ((value & 0x3ff) << 10) + (extra & 0x3ff) + 0x10000;
+							}
+						}
+						if (value >= 0xd800 && value <= 0xdbff) {
+							continue;  // drop lone surrogate
+						}
+					}
+
+					// expand the buffer if we couldn't write 4 bytes
+					if (at + 4 > target.length) {
+						tlen += 8;  // minimum extra
+						tlen *= (1.0 + (pos / string.length) * 2);  // take 2x the remaining
+						tlen = (tlen >>> 3) << 3;  // 8 byte offset
+
+						const update = new Uint8Array(tlen);
+						update.set(target);
+						target = update;
+					}
+
+					if ((value & 0xffffff80) === 0) {  // 1-byte
+						target[at++] = value;  // ASCII
+						continue;
+					} else if ((value & 0xfffff800) === 0) {  // 2-byte
+						target[at++] = ((value >>>  6) & 0x1f) | 0xc0;
+					} else if ((value & 0xffff0000) === 0) {  // 3-byte
+						target[at++] = ((value >>> 12) & 0x0f) | 0xe0;
+						target[at++] = ((value >>>  6) & 0x3f) | 0x80;
+					} else if ((value & 0xffe00000) === 0) {  // 4-byte
+						target[at++] = ((value >>> 18) & 0x07) | 0xf0;
+						target[at++] = ((value >>> 12) & 0x3f) | 0x80;
+						target[at++] = ((value >>>  6) & 0x3f) | 0x80;
+					} else {
+						continue;  // out of range
+					}
+
+					target[at++] = (value & 0x3f) | 0x80;
+				}
+
+				// Use subarray if slice isn't supported (IE11). This will use more memory
+				// because the original array still exists.
+				return target.slice ? target.slice(0, at) : target.subarray(0, at);
+			};
+
+			return GLibTextEncoder;
+		};
+		if (!global.TextEncoder) {
+			const GLib = imports.gi.GLib;
+			global.TextEncoder = polyfillTextEncoder();
+			TextEncoder = global.TextEncoder;
+		}
+		const polyfillTextDecoder = () => {
+			// Shameless plug from https://github.com/samthor/fast-text-encoding/blob/master/text.js
+			/**
+			 * @constructor
+			 * @param {string=} utfLabel
+			 * @param {{fatal: boolean}=} options
+			 */
+			function GLibTextDecoder(utfLabel='utf-8', options={fatal: false}) {
+				if (!(this instanceof GLibTextDecoder)) {
+					throw TypeError('Called as a function. Did you forget \'new\'?');
+				}
+
+				Object.defineProperty(GLibTextDecoder.prototype, 'encoding', {value: utfLabel});
+
+				if (options.fatal) {
+					throw new Error(`Failed to construct 'TextDecoder': the 'fatal' option is unsupported.`);
+				}
+			}
+
+			Object.defineProperty(GLibTextDecoder.prototype, 'fatal', {value: false});
+
+			Object.defineProperty(GLibTextDecoder.prototype, 'ignoreBOM', {value: false});
+
+			let decodeImpl = (bytes) => {
+				let res = GLib.convert(bytes, GLibTextDecoder.encoding, "utf-16");
+				return new ByteArray.ByteArray(res.return_value).toString();
+			};
+			/**
+			 * @param {(!ArrayBuffer|!ArrayBufferView)} buffer
+			 * @param {{stream: boolean}=} options
+			 * @return {string}
+			 */
+			GLibTextDecoder.prototype['decode'] = function(buffer, options={stream: false}) {
+				if (options['stream']) {
+					throw new Error(`Failed to decode: the 'stream' option is unsupported.`);
+				}
+
+				let bytes;
+
+				if (buffer instanceof Uint8Array) {
+					// Accept Uint8Array instances as-is.
+					bytes = buffer;
+				} else if (buffer.buffer instanceof ArrayBuffer) {
+					// Look for ArrayBufferView, which isn't a real type, but basically
+					// represents all the valid TypedArray types plus DataView. They all have
+					// ".buffer" as an instance of ArrayBuffer.
+					bytes = new Uint8Array(buffer.buffer);
+				} else {
+					// The only other valid argument here is that "buffer" is an ArrayBuffer.
+					// We also try to convert anything else passed to a Uint8Array, as this
+					// catches anything that's array-like. Native code would throw here.
+					bytes = new Uint8Array(buffer);
+				}
+
+				return decodeImpl(/** @type {!Uint8Array} */ (bytes));
+			};
+			return GLibTextDecoder;
+		};
+
+		if (!global.TextDecoder) {
+			const GLib = imports.gi.GLib;
+			global.TextDecoder = polyfillTextDecoder();
+			TextDecoder = global.TextDecoder;
+		}
 	}
 
 	if (!global.require && typeof require !== "undefined") {
